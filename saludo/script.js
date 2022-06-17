5c4abd61486b9dfaca6be9670559bd36531d8db8
emperatrizm <emperatriz.668@gmail.com>
2022-06-17 15:36:54 -0500

    innner para cambio h2

--- saludo/script.js
+++ saludo/script.js
@@ -9,13 +9,17 @@ setInterval(cambiarSaludo, 2000);
 function cambiarSaludo() {
 
   let frase = document.getElementsByTagName("h3")[0];
-console.log(frase)
+console.log(frase);
+
   if (frase.innerText == "Ganando como siempre") {
 
-    frase.innerText = "Impongo moda para las niñas!";
-  } else {
+    (frase.innerText = "Impongo moda para las niñas!");
+  } 
+  else {
  
-    frase.innerText = "Ganando como siempre";
+    (frase.innerText = "Ganando como siempre");
   }
   //console.log("frase :" + frase);
 }
+
+console.log(cambiarSaludo())
